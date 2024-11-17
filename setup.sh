990cf92926ffc6638ba85da4684c6b447220d0b5
vgr <neilalfonzcasas@gmail.com>
2024-11-17 19:21:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,76 @@ if [ $? -ne 0 ]; then
 fi
 echo "Finished updating system."
 
+# Installing curl
+echo "Installing curl..."
+sudo apt install curl -y
+if [ $? -ne 0 ]; then
+  echo "Failed to install curl. Exiting script."
+  exit 1
+fi
+echo "Finished installing curl."
+
+# Install pip (Python's package installer)
+echo "Installing pip..."
+sudo apt install python3-pip -y
+if [ $? -ne 0 ]; then
+  echo "Failed to install pip. Exiting script."
+  exit 1
+fi
+echo "Successfully installed pip."
+
+
+# Install NVM and Node.js
+echo "Installing NVM and Node.js..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+if [ $? -ne 0 ]; then
+  echo "Failed to install NVM. Exiting script."
+  exit 1
+fi
+
+# Source the profile to make nvm command available
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm install --lts
+if [ $? -ne 0 ]; then
+  echo "Failed to install Node.js. Exiting script."
+  exit 1
+fi
+
+nvm use --lts
+echo "Successfully installed Node.js."
+
+# Set up git
+echo "Setting up Git..."
+git config --global init.defaultBranch main
+git config --global color.ui auto
+git config --global pull.rebase false
+
+git config --global user.name "Neil Casas"
+git config --global user.email "neilalfonzcasas@gmail.com"
+
+echo "Successfully set Git username and email:"
+git config --get user.name
+git config --get user.email
+
+# Install Neovim
+echo "Installing Neovim..."
+sudo apt install neovim -y
+if [ $? -ne 0 ]; then
+  echo "Failed to install Neovim. Exiting script."
+fi
+echo "Finished installing Neovim."
+
+# Install Zsh
+echo "Installing Zsh..."
+sudo apt install zsh -y
+if [ $? -ne 0 ]; then
+  echo "Failed to install Zsh. Exiting script."
+  exit 1
+fi
+echo "Finished installing Zsh."
+
 if [[ "$isgui" == "Y" ]] || [[ "$isgui" == "y" ]]; then
 
 	# Installing flatpak
@@ -111,78 +181,6 @@ if [[ "$isgui" == "Y" ]] || [[ "$isgui" == "y" ]]; then
       	fi
 
 fi
-
-
-# Installing curl
-echo "Installing curl..."
-sudo apt install curl -y
-if [ $? -ne 0 ]; then
-  echo "Failed to install curl. Exiting script."
-  exit 1
-fi
-echo "Finished installing curl."
-
-# Install pip (Python's package installer)
-echo "Installing pip..."
-sudo apt install python3-pip -y
-if [ $? -ne 0 ]; then
-  echo "Failed to install pip. Exiting script."
-  exit 1
-fi
-echo "Successfully installed pip."
-
-
-# Install NVM and Node.js
-echo "Installing NVM and Node.js..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-if [ $? -ne 0 ]; then
-  echo "Failed to install NVM. Exiting script."
-  exit 1
-fi
-
-# Source the profile to make nvm command available
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
-nvm install --lts
-if [ $? -ne 0 ]; then
-  echo "Failed to install Node.js. Exiting script."
-  exit 1
-fi
-
-nvm use --lts
-echo "Successfully installed Node.js."
-
-# Set up git
-echo "Setting up Git..."
-git config --global init.defaultBranch main
-git config --global color.ui auto
-git config --global pull.rebase false
-
-git config --global user.name "Neil Casas"
-git config --global user.email "neilalfonzcasas@gmail.com"
-
-echo "Successfully set Git username and email:"
-git config --get user.name
-git config --get user.email
-
-# Install Neovim
-echo "Installing Neovim..."
-sudo apt install neovim -y
-if [ $? -ne 0 ]; then
-  echo "Failed to install Neovim. Exiting script."
-fi
-echo "Finished installing Neovim."
-
-# Install Zsh
-echo "Installing Zsh..."
-sudo apt install zsh -y
-if [ $? -ne 0 ]; then
-  echo "Failed to install Zsh. Exiting script."
-  exit 1
-fi
-echo "Finished installing Zsh."
-
 
 if [[ "$isdev" == "Y" ]] || [[ "$isdev" == "y" ]]; then 
 	# Installing VSCode for Debian/Ubuntu-based distros
